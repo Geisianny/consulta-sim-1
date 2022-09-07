ad2df7879e5454f7cc8f2a0a6d42d32c977cac80
Geisianny <geisiannybernardo@gmail.com>
2022-09-07 14:56:43 -0300

    Criando os testes do model Paciente

--- Gemfile
+++ Gemfile
@@ -4,10 +4,14 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.1.1"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 7.0.2", ">= 7.0.2.2"
+gem "rails", "~> 7.0.2", "= 7.0.2.2"
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
+
+gem "ffi"
+
+gem "rspec"
 
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.4"
@@ -80,7 +84,9 @@ def testing_gems
 end
 
 group :test do
-  testing_gems
+  gem "capybara"
+  gem "selenium-webdriver"
+  gem "webdrivers"
 end
 
 group :production, :actions do
--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "nao deve salvar paciente sem nome" do
+    paciente = Paciente.new nome: "", data_nascimento: "07-09-1999", cpf:"12345678910",
+     email: "pacientesemnome@gmail.com", endereco_attributes:{cidade: "Garanhuns", logradouro: "Rua Ufape",
+    complemento: "Apartamento", bairro: "Boa vista", cep: "55555-333"}
+
+    assert_not paciente.save
+  end
+
+  test "nao deve salvar paciente sem endereco" do 
+    paciente = Paciente.new nome: "paciente 1", data_nascimento: "12-09-1999", cpf:"12345678911",
+    email: "pacientesemendereco@gmail.com", endereco_attributes:{cidade: "", logradouro: "",
+    complemento: "", bairro: "", cep: "-"}
+
+    assert_not paciente.save
+  end
 end
