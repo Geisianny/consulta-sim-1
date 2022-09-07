dfec6cfabe8de9346cea1410f146a26736b2780e
Geisianny <geisiannybernardo@gmail.com>
2022-09-07 15:26:47 -0300

    Criando testes do model Medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -6,4 +6,17 @@ class MedicoTest < ActiveSupport::TestCase
                         especialidade: 'Reumatologista', crm: '123456'
     assert medico.save
   end
+
+  test 'Nao deve criar medico com cpf invalido' do
+    medico = Medico.new nome: 'medico sem cpf', cpf: '888.769.456-tt', email: 'medicocpf@gmail.com',
+                        especialidade: 'Cardiologia', crm: '222222'
+    assert_not medico.save
+  end
+
+  test 'Nao deve criar medico sem especialidade' do
+    medico = Medico.new nome: 'medico sem especialidade', cpf: '666.666.666-66', email: 'medicosemespecialidade@gmail.com',
+                        especialidade: '', crm: '111111'
+    assert_not medico.save
+  end
+  
 end
